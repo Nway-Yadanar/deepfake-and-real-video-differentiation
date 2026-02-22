9c76d3fbd7ae4845072c4121448e0fd94fc60c75
Nway Yadanar Aung <90437940+Nway-Yadanar@users.noreply.github.com>
2026-02-22 11:24:02 +0630

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import torch
-print(f"PyTorch Version: {torch.__version__}")
-print(f"CUDA Available: {torch.cuda.is_available()}")
-print(f"CUDA Architecture List: {torch.cuda.get_arch_list()}")
-# This line will run a small operation on the GPU to confirm functionality
-print(torch.randn(1).cuda())
